a5fa34ecec7c0282cb069483c25019342990212e
ashu3110 <ashutoshbankey21306@gmail.com>
2025-07-21 18:06:55 +0530

    first commit

--- Components/news-list.js
+++ Components/news-list.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
+import Image from "next/image"
 
 export default function NewsListPage({news}){
     return(
         <ul className="news-list">
             {news.map((newsitem)=>(
                 <li key={newsitem.id}>
-                    <Link href={`/news/${newsitem.slug}`}>
-                    <img src={`https://ashutoshnewswebsite.s3.ap-south-1.amazonaws.com/${newsitem.image}`} alt={newsitem.title} />
+                    <Link href={`/news/${newsitem.slug}`}> 
+                    <Image src={`https://ashutoshnewswebsite.s3.ap-south-1.amazonaws.com/${newsitem.image}` } alt={newsitem.title} fill />
                     <span>{newsitem.title}</span>
                     </Link>
                 </li>
